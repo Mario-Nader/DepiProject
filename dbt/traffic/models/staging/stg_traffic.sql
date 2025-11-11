0a7cec04caa6a248f5bd6f2c77f14b0af9cbc178
Keyrollos12 <kerokoko500246@gmail.com>
2025-11-12 01:06:35 +0200

    running dbt

--- dbt/traffic/models/staging/stg_traffic.sql
+++ dbt/traffic/models/staging/stg_traffic.sql
@@ -1,13 +1,24 @@
-{{ config(materialized='table') }}
+{{ config(materialized='incremental') }}
+
 SELECT
     Time AS traffic_time,
     Date AS traffic_date,
-    [DayOfTheWeek] AS day_of_week,  -- <-- Use square brackets
+    [DayOfTheWeek] AS day_of_week,
     CarCount,
     BikeCount,
     BusCount,
     TruckCount,
     Total AS total_vehicles,
-    [TrafficSituation] AS traffic_situation -- <-- Use square brackets
+    [TrafficSituation] AS traffic_situation
+    -- (You will need a real timestamp column here)
 FROM
     {{ source('azure_sql_traffic', 'TrafficLog') }}
+
+{% if is_incremental() %}
+
+  -- This 'WHERE' clause only runs on incremental runs
+  -- It finds the max timestamp from *this* model (the target)
+  -- and only selects *new* rows from the *source*
+  WHERE timestamp_column > (SELECT max(timestamp_column) FROM {{ this }})
+
+{% endif %}
